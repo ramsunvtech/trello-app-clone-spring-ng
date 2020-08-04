5e8c2cc4aa085094563ec9fc16787830a10ee657
Venkatraman <ramsunvtech@gmail.com>
2020-08-04 22:42:22 +0800

    tech(base): #1 - Base Spring Boot SetUp

---
+++ services/src/test/java/io/trello/trelloapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.trello.trelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
