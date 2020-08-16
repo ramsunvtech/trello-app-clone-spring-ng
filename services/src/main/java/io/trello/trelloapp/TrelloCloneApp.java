dc8cba4139177170e15e1132328b37b6630f4da5
Venkatraman <ramsunvtech@gmail.com>
2020-08-16 23:05:20 +0800

    feat(api/annotations): #30 - Add Swagger Annotations with Rename Base Application

--- services/src/main/java/io/trello/trelloapp/TrelloCloneApp.java
+++ services/src/main/java/io/trello/trelloapp/TrelloCloneApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TrelloCloneApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TrelloCloneApp.class, args);
 	}
 
 }
