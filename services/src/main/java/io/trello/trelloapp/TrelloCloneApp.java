2a58f16728282352e3c38facc9ffdaba6b69867e
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-08-20 00:30:16 +0530

    feat(general/cors): #41 - Adds cors config to allow api access for localhost

--- services/src/main/java/io/trello/trelloapp/TrelloCloneApp.java
+++ services/src/main/java/io/trello/trelloapp/TrelloCloneApp.java
@@ -1,13 +1,29 @@
 package io.trello.trelloapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrelloCloneApp {
+
+  @Value("${client.instance.local}")
+  private String apiDev;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrelloCloneApp.class, args);
 	}
 
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(apiDev);
+			}
+		};
+	}
 }
