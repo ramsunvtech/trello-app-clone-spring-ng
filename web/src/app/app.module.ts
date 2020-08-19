9c12d322b9ee2f7b1f9a8acd9d23b8de5290505a
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2020-08-19 11:57:58 +0530

    feat(component/card): #34  - Adds card component (#37)

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppHeaderComponent, AppBoardListComponent, AppBoardComponent, AppBoardFormComponent } from './components';
+import {
+  AppHeaderComponent,
+  AppBoardListComponent,
+  AppBoardComponent,
+  AppBoardFormComponent,
+  AppCardComponent,
+} from './components';
 
 import { Store } from '../store';
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent, AppBoardListComponent, AppBoardComponent, AppBoardFormComponent],
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppBoardListComponent,
+    AppBoardComponent,
+    AppBoardFormComponent,
+    AppCardComponent,
+  ],
   imports: [BrowserModule, FormsModule, Store],
   providers: [],
   bootstrap: [AppComponent],
---
+++ web/src/common/models/card.ts
@@ -0,0 +1,6 @@
+export interface CardItem {
+  id: number;
+  boardId: number;
+  title: string;
+  description: string;
+}
