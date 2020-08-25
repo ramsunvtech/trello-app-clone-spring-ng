6d0a100209fe6af66a00383ce4a187eba69aa556
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2020-08-25 16:17:52 +0530

    feat(services/card): #44 - Adds card services (#50)

---
+++ web/src/app/services/card.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// api
+import { environment } from 'src/environments/environment';
+
+// models
+import { CardItem } from 'src/common/models/card';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CardService {
+  constructor(private http: HttpClient) {}
+
+  getCards(): Observable<object> {
+    return this.http.get(environment.apiUrl + '/api/card-list');
+  }
+
+  addCard(card: CardItem): Observable<object> {
+    return this.http.post(environment.apiUrl + '/api/new-card', card);
+  }
+
+  editCard(card: CardItem): Observable<object> {
+    return this.http.post(environment.apiUrl + '/api/edit-card', card);
+  }
+
+  deleteCard(cardId: number): Observable<object> {
+    return this.http.delete(environment.apiUrl + '/api/delete-card/' + cardId);
+  }
+}
