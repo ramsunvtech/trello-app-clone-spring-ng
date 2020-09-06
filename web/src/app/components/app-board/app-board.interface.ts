de0a380ce8040bd06806361c9aa77203c4a29597
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-09-07 01:20:42 +0530

    feat(ui/drag-and-drop): #57 - Adds basic drag and drop using angular cdk

--- web/src/app/components/app-board/app-board.interface.ts
+++ web/src/app/components/app-board/app-board.interface.ts
@@ -1,8 +1,6 @@
-import { Observable } from 'rxjs';
-
 import { BoardItem, CardItem } from 'src/common/models';
 
 export interface AppBoardComponentInterface {
   boardItem: BoardItem;
-  cardList: Observable<CardItem[]>;
+  cardList: CardItem[];
 }
